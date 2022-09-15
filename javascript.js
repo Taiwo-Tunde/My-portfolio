3b65831c0d34f5fa1ac469bac86f42e58cb23f11
Taiwo <taiwotunde32@gmail.com>
2022-09-15 19:16:26 +0100

    portfolio

---
+++ javascript.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById('email').addEventListener('onClick', function(){
+    return (  href="https://twitter.com/rashy606?t=NbGyMrcWqnhuxeRNX9Hzw&s=09" )
+})
+
+
+
+console.log('taiwo')
